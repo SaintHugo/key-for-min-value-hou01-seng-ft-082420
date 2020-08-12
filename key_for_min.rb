a1326955ed1987f383927f59ef4e7f676eab2e6b
Hugo Rodrigez <hugor37@gmail.com>
2020-08-12 17:04:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-min = 1000
+
 def key_for_min_value(name_hash)
+  min = 1000
 name_hash.count do |key, value|
   if name_hash.count > min
 min = name_hash.count
